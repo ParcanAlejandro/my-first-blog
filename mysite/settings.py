9f43cd22185d5f3ec1cbb323603893da8d447027
Alejandro <amorales@parcan.es>
2018-10-03 13:41:33 +0100

    Cambie el HTML para la página.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '*g1lfl-ha*c@lk6^aho@(mveyvjd(19qtwa3t2sz7#m)rdkyh0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['parcanalejandro.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'parcanalejandro.pythonanywhere.com',
+    '127.0.0.1',
+    ]
 
 
 # Application definition
